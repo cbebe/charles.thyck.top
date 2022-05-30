506f963c690e933595358e274fa4c19c564ec2c5
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-05-29 23:56:54 -0600

    Add softie + make name responsive

--- src/components/basics/AnimatedSVG.tsx
+++ src/components/basics/AnimatedSVG.tsx
@@ -6,22 +6,13 @@ export interface AnimatedSVGProps {
   pathDs: string[];
   strokeWidth?: `${number}`;
   stroke?: string;
-  width?: number;
-  height?: number;
 }
 
 export function AnimatedSVG(props: AnimatedSVGProps) {
-  const { viewBox, pathDs, strokeWidth = "5", stroke = "white", width = 400, height = 250 } = props;
+  const { viewBox, pathDs, strokeWidth = "5", stroke = "white" } = props;
 
   return (
-    <svg
-      className={styles.svg}
-      width={width}
-      height={height}
-      viewBox={viewBox}
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
+    <svg className={styles.svg} viewBox={viewBox} fill="none" xmlns="http://www.w3.org/2000/svg">
       {pathDs.map((v, idx) => (
         <path key={idx} d={v} stroke={stroke} strokeWidth={strokeWidth} />
       ))}
--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -1,4 +1,5 @@
 import Layout from "@theme/Layout";
+import clsx from "clsx";
 import React from "react";
 import { Name } from "./Name";
 import styles from "./styles.module.css";
@@ -6,8 +7,9 @@ import styles from "./styles.module.css";
 export function Home(): JSX.Element {
   return (
     <Layout wrapperClassName={styles.wrapper} title="Hello!" description="Charles Ancheta's Personal Website">
-      <main className="centre-content">
+      <main className={clsx("centre-content", styles.main)}>
         <Name />
+        <h1 className={clsx("hero__subtitle", styles.softie)}>Software Engineer</h1>
       </main>
     </Layout>
   );

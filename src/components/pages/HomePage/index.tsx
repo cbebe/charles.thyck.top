ca8678f1a105fe356c2f707a00d6191bcc65634d
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-05-30 00:17:19 -0600

    Add VMoji (victory emoji)

--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -3,13 +3,17 @@ import clsx from "clsx";
 import React from "react";
 import { Name } from "./Name";
 import styles from "./styles.module.css";
+import { VMoji } from "./VMoji";
 
 export function Home(): JSX.Element {
   return (
     <Layout wrapperClassName={styles.wrapper} title="Hello!" description="Charles Ancheta's Personal Website">
       <main className={clsx("centre-content", styles.main)}>
         <Name />
-        <h1 className={clsx("hero__subtitle", styles.softie)}>Software Engineer</h1>
+        <div className={styles.softieWrapper}>
+          <VMoji />
+          <h1 className={clsx("hero__subtitle", styles.softie)}>Software Engineer</h1>
+        </div>
       </main>
     </Layout>
   );

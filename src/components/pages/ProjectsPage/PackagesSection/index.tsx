96b6b1419b79408c25ee5bb69d42e155efcad3d1
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-06-07 04:49:01 -0600

    Add packages on build time

--- src/components/pages/ProjectsPage/PackagesSection/index.tsx
+++ src/components/pages/ProjectsPage/PackagesSection/index.tsx
@@ -1,18 +1,46 @@
-import React from "react";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import { NPMPackage } from "@site/config/package";
+import React, { useMemo } from "react";
 
 interface Package {
-  name: "";
-  url: "";
-  description: "";
+  name: string;
+  url: string;
+  description: string;
 }
 
-const packages = [];
+function usePackages(): Package[] {
+  const {
+    siteConfig: {
+      customFields: { packages },
+    },
+  } = useDocusaurusContext();
+  const data = useMemo<Package[]>(
+    () =>
+      (packages as NPMPackage[]).map((p) => ({
+        name: p.package.name,
+        url: p.package.links.npm,
+        description: p.package.description,
+      })),
+    [packages]
+  );
+  return data;
+}
 
 export function PackagesSection() {
+  const packages = usePackages();
   return (
     <section id="packages">
       <div className="text--center">
         <h1 className="hero__title">NPM Packages</h1>
+        {packages.map((p) => (
+          <div key={p.name}>
+            <h1>
+              <Link href={p.url}>{p.name}</Link>
+            </h1>
+            <p>{p.description}</p>
+          </div>
+        ))}
       </div>
     </section>
   );

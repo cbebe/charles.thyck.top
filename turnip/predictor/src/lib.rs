908c89bd1127676f8559bc90a1da628ac73f86b3
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-12-05 00:25:30 -0700

    Remove wee_alloc
    It doesn't really decrease my binary size :(

--- turnip/predictor/src/lib.rs
+++ turnip/predictor/src/lib.rs
@@ -12,12 +12,6 @@ use std::convert::TryInto;
 use wasm_bindgen::prelude::wasm_bindgen;
 use wasm_bindgen::JsValue;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[derive(Debug, PartialEq)]
 pub enum PredictorError {
     BadBuyPrice,
